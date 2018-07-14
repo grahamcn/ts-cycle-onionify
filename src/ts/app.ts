f62d163665f293ce4d6c4488d8620483a09d51ab
Graham Neilson <graham.c.neilson@gmail.com>
2018-07-14 20:16:34 +0100

    nested components

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,26 +1,54 @@
 import xs from 'xstream'
 import { run } from '@cycle/run'
+import isolate from '@cycle/isolate'
+import onionify from 'cycle-onionify'
 import { makeDOMDriver, div, p, h2 } from '@cycle/dom'
 import '../scss/index.scss'
-import onionify from 'cycle-onionify'
+import Child from './child'
 
-function main(sources) {
+function main(sources): any {
 	const state$ = sources.onion.state$
 
+	// 'child'
+	// The property child will host the state for the child component. The parent component
+	// needs to isolate the child component under the scope 'child'
+	const childSinks = isolate(Child, 'child')(sources)
+
 	const initialReducer$ =
-		xs.of(function initialReducer() { return 0 })
+		xs.of(function initialReducer() {
+			return {
+				elapsed: 0,
+			}
+		})
 
 	const addOneReducer$ =
 		xs.periodic(1000)
-			.mapTo(function addOneReducer(prev) { return prev + 1 })
+			.mapTo(function addOneReducer(prev) {
+				return Object.assign({}, {...prev}, {
+					elapsed: prev.elapsed + 1,
+				})
+			})
 
-	const reducer$ = xs.merge(initialReducer$, addOneReducer$)
+	const parentReducer$ = xs.merge(initialReducer$, addOneReducer$)
+	const childReducer$ = childSinks.onion
+	const reducer$ = xs.merge(parentReducer$, childReducer$)
 
-	const vdom$ =
+	const parentDOM$ =
 		state$.map(state =>
 			div([
 				h2('App Level State'),
 				p(JSON.stringify(state)),
+			])
+		)
+
+	const vdom$ =
+		xs.combine(
+			parentDOM$,
+			childSinks.DOM
+		).map(([parentDOM, childDom]) =>
+			div([
+				parentDOM,
+				childDom,
 			])
 		)
 
---
+++ src/ts/child.ts
@@ -0,0 +1,49 @@
+import xs from 'xstream'
+import { button, div, h3, p } from '@cycle/dom'
+
+function Child(sources): any {
+	const state$ = sources.onion.state$
+
+	const defaultReducer$ = xs.of(function defaultReducer(prevState) {
+		// Parent didn't provide state for the child, so initialize it, or use the state given from the parent.
+		return (typeof prevState === 'undefined') ? 0 : prevState
+	})
+
+	// this reducer acts upon the 'child' piece of state, as the Child component
+	// is isolated by the Parent under the scope 'child'
+	// child is a primitive in this example (number), so the initial / default reducer and
+	// all other reducers merged must act on and return that piece of state.
+	// state for a component would be defined via an interface.
+	const incrementReducer$ =
+		sources.DOM
+			.select('.increment')
+			.events('click')
+			.mapTo(function incrementReducer(prev) { return prev + 1 })
+
+	const reducer$ = xs.merge(defaultReducer$, incrementReducer$)
+
+	const vdom$ =
+		state$.map(state =>
+			div([
+				div([
+					h3('Child Level State'),
+					p(JSON.stringify(state)),
+				]),
+				button('.increment', {
+					attrs: {
+						type: 'button',
+					},
+					style: {
+						border: 'solid 1px rgba(0,0,0,0.8)',
+					},
+				}, 'Increment child state')
+			])
+		)
+
+	return {
+		DOM: vdom$,
+		onion: reducer$,
+	}
+}
+
+export default Child
